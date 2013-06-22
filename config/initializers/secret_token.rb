9f56f87fd4ca25715894addf537a29b144688372
Arthur Gunn <arthur@gunn.co.nz>
2013-06-23 03:14:26 +1200

    Default rails 4rc2 setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hybridise::Application.config.secret_key_base = 'a33eebba6ebe41e2d339aedefe19e5872a61720d2fbdda751e58693f2e9345d7cb654f195071219df86dea5434e9529ec73d483b862b45fe0e02fedb66bd8859'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hybridise::Application.config.session_store :cookie_store, key: '_hybridise_session'
